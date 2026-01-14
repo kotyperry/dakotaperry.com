48d8b096cbd6d25dcc574552dfae86c6320519db
Koty Perry <me@dakotaperry.com>
2026-01-14 15:17:42 -0600

    Refactor imports for consistency and clarity; update OpenPanel configuration with API URL.

--- src/openpanel.ts
+++ src/openpanel.ts
@@ -1,6 +1,7 @@
 import { OpenPanel } from '@openpanel/web'
 
 export const op = new OpenPanel({
+  apiUrl: 'https://openpanel.blkdog.dev',
   clientId: '368d49f1-4272-479e-ab82-c39efae48f0b',
   trackScreenViews: true,
   trackOutgoingLinks: true,
