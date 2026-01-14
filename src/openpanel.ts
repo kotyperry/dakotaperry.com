55c5cb27158633e182f9f2358f4d7a0064e6d43c
Koty Perry <me@dakotaperry.com>
2026-01-14 15:46:08 -0600

    Update OpenPanel configuration with new API URL and standardize import syntax.

--- src/openpanel.ts
+++ src/openpanel.ts
@@ -1,9 +1,9 @@
-import { OpenPanel } from '@openpanel/web'
+import { OpenPanel } from "@openpanel/web";
 
 export const op = new OpenPanel({
-  apiUrl: 'https://openpanel.blkdog.dev',
-  clientId: '368d49f1-4272-479e-ab82-c39efae48f0b',
+  apiUrl: "https://api.blkdog.dev",
+  clientId: "368d49f1-4272-479e-ab82-c39efae48f0b",
   trackScreenViews: true,
   trackOutgoingLinks: true,
   trackAttributes: true,
-})
+});
